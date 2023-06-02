e3c6cc46033997341935c9aecec1aad3840a5812
haoziqaq <357229046@qq.com>
2023-06-03 01:21:49 +0800

    fix: fix env

--- vite.config.ts
+++ vite.config.ts
@@ -5,31 +5,33 @@ import { fileURLToPath, URL } from 'node:url'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      '~': fileURLToPath(new URL('./src/assets', import.meta.url))
-    }
-  },
-  plugins: [
-    vue({
-      template: {
-        transformAssetUrls: {
-          'var-image': ['src'],
-          'var-avatar': ['src'],
-          'var-card': ['src'],
-          'var-app-bar': ['image']
-        }
+export default defineConfig(() => {
+  return {
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        '~': fileURLToPath(new URL('./src/assets', import.meta.url))
       }
-    }),
-    components({
-      resolvers: [VarletUIResolver()]
-    }),
-    autoImport({
-      imports: ['vue', 'vue-router', 'pinia'],
-      resolvers: [VarletUIResolver({ autoImport: true })],
-      eslintrc: { enabled: true }
-    })
-  ]
+    },
+    plugins: [
+      vue({
+        template: {
+          transformAssetUrls: {
+            'var-image': ['src'],
+            'var-avatar': ['src'],
+            'var-card': ['src'],
+            'var-app-bar': ['image']
+          }
+        }
+      }),
+      components({
+        resolvers: [VarletUIResolver()]
+      }),
+      autoImport({
+        imports: ['vue', 'vue-router', 'pinia'],
+        resolvers: [VarletUIResolver({ autoImport: true })],
+        eslintrc: { enabled: true }
+      })
+    ]
+  }
 })
