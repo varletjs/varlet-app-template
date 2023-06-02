62e7d511f7a2b56279709c68eb19a1a2e79c2071
ayang <75017711+ayangweb@users.noreply.github.com>
2023-06-02 21:39:05 +0800

    chore: configuration optimization (#2)

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,21 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import autoImport from 'unplugin-auto-import/vite'
+import components from 'unplugin-vue-components/vite'
+import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      include: [/\.[tj]sx?$/, /\.vue$/, /\.vue\?vue/, /\.md$/],
-      imports: ["vue", "vue-router", "pinia"],
-      dts: "src/auto-import.d.ts",
+    autoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
       eslintrc: {
-        enabled: false, // 1、改为true用于生成eslint配置。2、生成后改回false，避免重复生成消耗
-        filepath: './.eslintrc-auto-import.json', // 解决eslint报错问题
-        globalsPropValue: true
+        enabled: true
       },
+      resolvers: [VarletUIResolver({ autoImport: true })]
     }),
-    Components({
-      dirs: ["src/components"], // 配置需要默认导入的自定义组件文件夹，该文件夹下的所有组件都会自动 import
-      resolvers: [
-
-      ],
-    }),
-  ],
-});
+    components({
+      resolvers: [VarletUIResolver()]
+    })
+  ]
+})
