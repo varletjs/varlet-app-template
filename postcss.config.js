e5817fae5b2ca97e052654d96fa00f400434497f
haoziqaq <357229046@qq.com>
2023-06-03 01:08:58 +0800

    chore: integrated postcss-px-to-viewport

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+export default {
+  plugins: {
+    'postcss-px-to-viewport': {
+      viewportWidth: 375,
+      viewportUnit: 'vmin',
+      fontViewportUnit: 'vmin',
+      unitPrecision: 6,
+      unitToConvert: 'px',
+      propList: ['*']
+    }
+  }
+}
