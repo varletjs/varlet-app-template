03d0d77a07b03fbe0b7fb6c77710a35126412044
gaoting <837082729@qq.com>
2023-06-04 12:41:22 +0800

    feat: 合并

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const msg = ref('按钮')
+</script>
 
 <template>
   <var-button>Varlet</var-button>
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoImport from 'unplugin-auto-import/vite'
 import components from 'unplugin-vue-components/vite'
 import { VarletUIResolver } from 'unplugin-vue-components/resolvers'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [
@@ -19,6 +19,11 @@ export default defineConfig({
     }),
     components({
       resolvers: [VarletUIResolver()]
+    }),
+    autoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      eslintrc: { enabled: true },
+      resolvers: [VarletUIResolver({ autoImport: true })]
     })
   ],
   resolve: {
