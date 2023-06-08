c5ac86970e65b9deda5029eea32d98a60f7ab5fa
gaoting <837082729@qq.com>
2023-06-08 13:38:34 +0800

    feat: vitest + router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,27 @@
 import { createRouter, createWebHistory, Router } from 'vue-router'
 import routes from './routes'
+import Layout from '../views/layout/Layout.vue'
 
 const router: Router = createRouter({
-  history: createWebHistory('/'),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       redirect: '/home',
-      component: () => import('@/views/layout/Layout.vue'),
+      component: Layout,
       children: [
         ...routes,
         {
           path: '/403',
           name: '403',
           meta: { public: true, title: '403' },
-          component: () => import(/* webpackChunkName: "common" */ '@/views/notPage/Page403.vue')
+          component: () => import(/* webpackChunkName: "common" */ '../views/not-page/StatusForbidden.vue')
         },
         {
           path: '/:pathMatch(.*)',
           name: '404',
           meta: { public: true, title: '404' },
-          component: () => import(/* webpackChunkName: "common" */ '@/views/notPage/Page404.vue')
+          component: () => import(/* webpackChunkName: "common" */ '../views/not-page/StatusNotFound.vue')
         }
       ]
     }
