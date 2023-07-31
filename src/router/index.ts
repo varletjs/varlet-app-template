1f315830d4b75502a9767b61cc6a088638ce18be
haoziqaq <357229046@qq.com>
2023-07-31 16:58:32 +0800

    style: remove console

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,6 @@
 import routes from '~pages'
 import { createRouter, createWebHashHistory, Router } from 'vue-router'
 
-console.log(routes)
-
 const router: Router = createRouter({
   history: createWebHashHistory(),
   routes: [
