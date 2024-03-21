44c3e920f03dfd0f29dc4db267bd3edfa22c529a
haoziqaq <357229046@qq.com>
2024-03-22 01:39:03 +0800

    feat: support more sizes

--- uno.config.ts
+++ uno.config.ts
@@ -19,7 +19,12 @@ export default defineConfig({
           xl: '18px',
           '2xl': '20px',
           '3xl': '22px',
-          '4xl': '24px'
+          '4xl': '24px',
+          '5xl': '26px',
+          '6xl': '28px',
+          '7xl': '30px',
+          '8xl': '32px',
+          '9xl': '34px'
         }
 
         return {
