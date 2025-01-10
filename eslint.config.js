96231473a9e5b8c56a6012855f9f2a523d08ef21
haoziqaq <357229046@qq.com>
2025-01-10 17:08:17 +0800

    style: cleanup

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,11 @@
 import { defineConfig } from '@configurajs/eslint'
+import autoImport from './.eslintrc-auto-import.js'
 
 export default defineConfig({
   ignores: ['typed-router.d.ts'],
+  overrides: [
+    {
+      languageOptions: autoImport,
+    },
+  ],
 })
